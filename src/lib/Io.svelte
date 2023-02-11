19ce1b039f1b0f082d32cbc1b280c9d0084ced7a
BrittleFoot <buftioo@gmail.com>
2023-02-12 00:46:04 +0700

    true autoplay

--- src/lib/Io.svelte
+++ src/lib/Io.svelte
@@ -1,10 +1,10 @@
 <!-- svelte-ignore a11y-media-has-caption -->
-<script>
+<script lang="ts">
 	let preview = '/wisp.png';
 	let animation = '/wisp.webm';
 </script>
 
-<video class="hero" poster={preview} preload="auto" autoplay loop playsinline>
+<video id="hero-anim" class="hero" poster={preview} preload="auto" autoplay loop playsinline muted>
 	<source type="video/webm" src={animation} />
 	<img src={preview} alt="IO" />
 </video>
