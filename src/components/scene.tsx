b1053094fa81353cb3dd89b6b91d4971b4dede6d
BrittleFoot <buftioo@gmail.com>
2025-01-30 20:11:07 +0400

    sitting duck

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,20 +1,80 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
-import { CameraControls, Environment, Sphere } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import { CameraControls, Environment, useGLTF } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
+import { useEffect, useRef } from 'react'
+import * as THREE from 'three'
 import { KeyControlsHandler, KeyControlsProvider } from './key'
 
-function SnakeScene() {
+function Monk() {
+    // Load the GLB file
+    const obj = useGLTF('/sitting.glb')
+    const { scene, animations } = obj
+
+    // Animation mixer reference
+    const mixer = useRef<THREE.AnimationMixer | null>(null)
+
+    useEffect(() => {
+        // Initialize the AnimationMixer when the component mounts
+        mixer.current = new THREE.AnimationMixer(scene)
+
+        // Play the first animation clip
+        const action = mixer.current.clipAction(animations[0])
+        action.play()
+
+        // Cleanup the mixer when the component unmounts
+        return () => {
+            mixer.current?.stopAllAction()
+        }
+    }, [scene, animations])
+
+    // Update the animation on every frame
+    useFrame((state, delta) => {
+        if (mixer.current) mixer.current.update(delta) // Advance animation based on time
+    })
+
+    return (
+        <mesh position={[0.0, 0.0, 0.0]}>
+            <primitive object={scene} />
+        </mesh>
+    )
+}
+
+function Rock() {
+    // Load the GLB file
+    const obj = useGLTF('/rock.glb')
+
+    return (
+        <mesh
+            scale={2}
+            position={[-1.8, -0.9, -0.3]}
+            rotation={[0, (7 * Math.PI) / 4, 0]}
+        >
+            <primitive object={obj.scene} />
+        </mesh>
+    )
+}
+
+function Olympus() {
+    return (
+        <>
+            <Monk />
+            <Rock />
+        </>
+    )
+}
+
+function _Scene() {
     const [mX, mY, mZ] = [100, 100, 100]
 
     return (
         <>
             <ambientLight intensity={0} />
-            <Sphere position={[0, 0, 0]} args={[2, 16, 16]}>
+            {/* <Sphere position={[0, 0, 0]} args={[2, 16, 16]}>
                 <meshStandardMaterial color="white" />
-            </Sphere>
+            </Sphere> */}
 
             <pointLight
                 position={[mX / 2, mY / 2, -mZ / 4]}
@@ -30,6 +90,8 @@ function SnakeScene() {
 
             <Environment preset="sunset" />
 
+            <Olympus />
+
             <EffectComposer>
                 <Bloom luminanceThreshold={0.5} intensity={0.1} />
             </EffectComposer>
@@ -41,7 +103,7 @@ export function Scene() {
     return (
         <KeyControlsProvider>
             <Canvas shadows>
-                <SnakeScene />
+                <_Scene />
 
                 <CameraControls makeDefault />
                 <KeyControlsHandler />
